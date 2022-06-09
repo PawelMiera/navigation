8d497d8bf532cac76abd1300ffb43bcef74e037b
PawelMiera <pawelmiera@gmail.com>
2022-06-09 17:28:50 +0200

    first commit

---
+++ python/laser.py
@@ -0,0 +1,55 @@
+import rospy
+from sensor_msgs.msg import LaserScan
+import cv2
+
+
+class Laser:
+    self.__init__(self):
+        self.ran
+        self.window_size = (1000, 1000)
+        self.window_size_half = (int(self.window_size[0] / 2), int(self.window_size[1] / 2))
+        self.pixels_per_meter = 50
+        self.laser_angle_per_step = 2 * pi / self.laser_resolution
+        self.laser_max_range = 10
+        self.laser_min_range = 0.15
+        self.laser_resolution = 360
+        self.laser_ranges = np.full(self.laser_resolution, self.laser_max_range, dtype=np.float32)
+        self.laser_data = np.full(self.laser_resolution, self.laser_max_range, dtype=np.float32)
+        
+
+    def laser_callback(self, msg):
+        self.laser_data = np.array(msg.ranges).astype(np.float32)
+
+
+    def preprocess_lasers(self):
+        data = self.laser_data
+        data[data == np.inf] = self.laser_max_range
+
+        data = data[::-1]
+        self.laser_ranges = data
+
+    def render(self, mode='human'):
+        background = np.zeros((self.window_size[0], self.window_size[1], 3), np.uint8)
+        background[:] = (0, 255, 0)
+
+        for i in range(self.laser_resolution):
+            A = int(self.laser_ranges[i] * self.pixels_per_meter * sin(i * self.laser_angle_per_step)) + \
+                self.window_size_half[0]
+            B = -int(self.laser_ranges[i] * self.pixels_per_meter * cos(i * self.laser_angle_per_step)) + \
+                self.window_size_half[1]
+            cv2.line(background, self.window_size_half, (A, B), (0, 0, 255), 1)
+            cv2.circle(background, (A, B), 2, (0, 0, 255), -1)
+
+        drone_x_min = self.window_size_half[0] - 15
+        drone_y_min = self.window_size_half[1] - 10
+        background = cv2.rectangle(background, (drone_y_min, drone_x_min), (drone_y_min + 20, drone_x_min + 30),
+                                   (0, 0, 0), -1)
+        background = cv2.rectangle(background, (drone_y_min, drone_x_min), (drone_y_min + 20, drone_x_min + 5),
+                                   (255, 0, 0), -1)
+
+        cv2.imshow("game", background)
+        return cv2.waitKey(1)
+
+rospy.init_node('scan_values')
+sub = rospy.Subscriber('/scan', LaserScan, callback)
+rospy.spin()
---
+++ python/offboard.py
@@ -0,0 +1,313 @@
+from __future__ import division
+
+import rospy
+import math
+import numpy as np
+from geometry_msgs.msg import PoseStamped, Quaternion, TwistStamped
+from mavros_msgs.msg import ParamValue, PositionTarget
+from mavros_test_common import MavrosTestCommon
+from pymavlink import mavutil
+from six.moves import xrange
+from std_msgs.msg import Header
+from threading import Thread
+from tf.transformations import quaternion_from_euler
+from pynput import keyboard
+
+from sensor_msgs.msg import LaserScan
+
+
+class Modes:
+    POSITION_CONTROL = 0
+    VELOCITY_CONTROL = 1
+    RL = 2
+
+
+class MavrosOffboardPosctlTest(MavrosTestCommon):
+
+    def setUp(self):
+        super(MavrosOffboardPosctlTest, self).setUp()
+
+        self.pos = PoseStamped()
+
+        self.vel_global = TwistStamped()
+
+        self.mode = Modes.POSITION_CONTROL
+
+        self.vel_local = PositionTarget()
+
+        self.vel_local.type_mask = PositionTarget.IGNORE_YAW
+
+        self.pos_setpoint_pub = rospy.Publisher(
+            '/mavros/setpoint_position/local', PoseStamped, queue_size=1)
+
+        self.vel_local_pub = rospy.Publisher(
+            '/mavros/setpoint_raw/local', PositionTarget, queue_size=1)             # prawdopodobnie da sie wszystkim sterowac
+
+        # send setpoints in seperate thread to better prevent failsafe
+        self.drone_control_thread = Thread(target=self.control_drone, args=())
+        self.drone_control_thread.daemon = True
+        self.drone_control_thread.start()
+
+        self.v_x = 0
+        self.v_y = 0
+        self.v_z = 0
+        self.v_yaw = 0
+        self.speed = 0.5
+
+        sub = rospy.Subscriber('/laser/scan', LaserScan, self.laser_callback)
+
+        self.laser_max_range = 10
+        self.laser_min_range = 0.15
+        self.laser_resolution = 360
+        
+
+        self.laser_ranges = np.full(self.laser_resolution, self.laser_max_range, dtype=np.float32)
+        self.laser_data = np.full(self.laser_resolution, self.laser_max_range, dtype=np.float32)
+
+        self.model = PPO.load("vec_model6")
+
+        self.yaw_i = 0
+        self.yaw_p = 1
+        self.yaw_p_i = 0.002
+
+        self.pos_z_i = 0
+        self.pos_z_p = 0.7
+        self.pos_z_p_i = 0.002
+
+        self.desired_heigth = 2.2
+        self.desired_yaw = 0.0
+
+    def tearDown(self):
+        super(MavrosOffboardPosctlTest, self).tearDown()
+
+
+    def preprocess_lasers(self):
+        data = self.laser_data
+        data[data == np.inf] = self.laser_max_range
+
+        data = data[::-1]
+        self.laser_ranges = data
+
+    def normalize_lasers(self, laser_ranges):
+        divider = self.laser_max_range / 2
+        laser_ranges = ((laser_ranges.copy() - divider) / divider).astype(np.float32)
+        return laser_ranges
+
+    def laser_callback(self, msg):
+        self.laser_data = np.array(msg.ranges).astype(np.float32)
+
+
+    #
+    # Helper methods
+    #
+    def control_drone(self):
+        rate = rospy.Rate(30)  # Hz
+        self.pos.header = Header()
+        self.pos.header.frame_id = "base_footprint"
+
+        self.vel_local.header = Header()
+        self.vel_local.header.frame_id = "base_footprint"
+
+        self.vel_global.header = Header()
+        self.vel_global.header.frame_id = "base_footprint"
+
+        while not rospy.is_shutdown():
+            if self.mode == Modes.POSITION_CONTROL:
+                self.pos.header.stamp = rospy.Time.now()
+                self.pos_setpoint_pub.publish(self.pos)
+            elif self.mode == Modes.VELOCITY_CONTROL:
+                self.vel_local.header.stamp = rospy.Time.now()
+                self.vel_local_pub.publish(self.vel_local)
+            elif self.mode == Modes.RL:
+                self.preprocess_lasers()
+
+                obs = self.normalize_lasers(self.laser_ranges)
+
+                action, _states = self.model.predict(obs, deterministic=True)
+
+                #rospy.loginfo(str(action))
+
+                e = (self.desired_yaw - self.local_position.pose.orientation.z)
+                p = e * self.yaw_p
+                self.yaw_i += self.yaw_p_i * e * (1 / 30)
+
+                o = p + self.yaw_i
+
+                e_z = self.desired_heigth - self.local_position.pose.position.z
+
+                p_z = e_z * self.pos_z_p
+                self.pos_z_i += self.pos_z_p_i * e_z * (1 / 30)
+
+                o_z = p_z + self.pos_z_i
+
+                self.set_velocity(action[0], -action[1], o_z, -o)
+                self.vel_local.header.stamp = rospy.Time.now()
+                self.vel_local_pub.publish(self.vel_local)
+
+
+            try:  # prevent garbage in console output when thread is killed
+                rate.sleep()
+            except rospy.ROSInterruptException:
+                pass
+
+    def is_at_position(self, x, y, z, offset):
+        """offset: meters"""
+        rospy.logdebug(
+            "current position | x:{0:.2f}, y:{1:.2f}, z:{2:.2f}".format(
+                self.local_position.pose.position.x, self.local_position.pose.
+                position.y, self.local_position.pose.position.z))
+
+        desired = np.array((x, y, z))
+        pos = np.array((self.local_position.pose.position.x,
+                        self.local_position.pose.position.y,
+                        self.local_position.pose.position.z))
+        return np.linalg.norm(desired - pos) < offset
+
+    def reach_position(self, x, y, z, timeout):
+        """timeout(int): seconds"""
+        # set a position setpoint
+        self.pos.pose.position.x = x
+        self.pos.pose.position.y = y
+        self.pos.pose.position.z = z
+        rospy.loginfo(
+            "attempting to reach position | x: {0}, y: {1}, z: {2} | current position x: {3:.2f}, y: {4:.2f}, z: {5:.2f}".
+            format(x, y, z, self.local_position.pose.position.x,
+                   self.local_position.pose.position.y,
+                   self.local_position.pose.position.z))
+
+        # For demo purposes we will lock yaw/heading to north.
+        yaw_degrees = 180  # North
+        yaw = math.radians(yaw_degrees)
+        quaternion = quaternion_from_euler(0, 0, yaw)
+        self.pos.pose.orientation = Quaternion(*quaternion)
+
+        # does it reach the position in 'timeout' seconds?
+        loop_freq = 2  # Hz
+        rate = rospy.Rate(loop_freq)
+        reached = False
+        for i in xrange(timeout * loop_freq):
+            if self.is_at_position(self.pos.pose.position.x,
+                                   self.pos.pose.position.y,
+                                   self.pos.pose.position.z, 0.5):
+                rospy.loginfo("position reached | seconds: {0} of {1}".format(
+                    i / loop_freq, timeout))
+                reached = True
+                break
+
+            try:
+                rate.sleep()
+            except rospy.ROSException as e:
+                self.fail(e)
+
+        self.assertTrue(reached, (
+            "took too long to get to position | current position x: {0:.2f}, y: {1:.2f}, z: {2:.2f} | timeout(seconds): {3}".
+            format(self.local_position.pose.position.x,
+                   self.local_position.pose.position.y,
+                   self.local_position.pose.position.z, timeout)))
+
+    def take_off(self, z, azimuth, timeout, max_error):
+        self.set_position(self.local_position.pose.position.x, self.local_position.pose.position.y, z, azimuth, timeout, max_error)
+
+    def set_velocity(self, v_x, v_y, v_z, v_yaw):
+
+        self.vel_local.velocity.x = v_x
+        self.vel_local.velocity.y = v_y
+        self.vel_local.velocity.z = v_z
+
+        self.vel_local.coordinate_frame = PositionTarget.FRAME_BODY_NED
+        self.vel_local.yaw_rate = v_yaw
+
+
+    def set_position(self, x, y, z, azimuth, timeout, max_error):
+        self.mode = Modes.POSITION_CONTROL
+        self.pos.pose.position.x = x
+        self.pos.pose.position.y = y
+        self.pos.pose.position.z = z
+
+        yaw = math.radians(azimuth)
+        quaternion = quaternion_from_euler(0, 0, yaw)
+        self.pos.pose.orientation = Quaternion(*quaternion)
+
+        loop_freq = 2  # Hz
+        rate = rospy.Rate(loop_freq)
+        reached = False
+        for i in xrange(timeout * loop_freq):
+            if self.is_at_position(self.pos.pose.position.x,
+                                   self.pos.pose.position.y,
+                                   self.pos.pose.position.z, max_error):
+                rospy.loginfo("position reached | seconds: {0} of {1}".format(
+                    i / loop_freq, timeout))
+                reached = True
+                break
+
+            try:
+                rate.sleep()
+            except rospy.ROSException as e:
+                self.fail(e)
+
+        self.assertTrue(reached, (
+            "took too long to get to position | current position x: {0:.2f}, y: {1:.2f}, z: {2:.2f} | timeout(seconds): {3}".
+            format(self.local_position.pose.position.x,
+                   self.local_position.pose.position.y,
+                   self.local_position.pose.position.z, timeout)))
+        return reached
+
+    def on_release(self, key):
+
+        if key == 'q':
+            return True
+        elif key == '1':
+            self.set_arm(True, 5)
+        elif key == '2':
+            self.set_arm(False, 5)
+        elif key == '3':
+            self.set_arm(True, 5)
+            rospy.loginfo("Start_RL")
+            self.take_off(2.5, 0, 20, 0.5)
+            self.mode = Modes.RL
+        if self.state.armed:
+            if key == 't':
+                self.take_off(2.5, 0, 20, 0.5)
+            elif key == 'p':
+                self.rtl()
+            elif key == 'l':
+                self.land()
+        return False
+
+
+
+    def land(self):
+        self.set_mode("AUTO.LAND", 5)
+
+    def rtl(self):
+        self.set_mode("AUTO.RTL", 5)
+
+
+    def test_posctl(self):
+        """Test offboard position control"""
+
+        # make sure the simulation is ready to start the mission
+        self.wait_for_topics(60)
+        self.wait_for_landed_state(mavutil.mavlink.MAV_LANDED_STATE_ON_GROUND,
+                                   10, -1)
+        self.log_topic_vars()
+        # exempting failsafe from lost RC to allow offboard
+        rcl_except = ParamValue(1<<2, 0.0)
+        self.set_param("COM_RCL_EXCEPT", rcl_except, 5)
+        self.set_mode("OFFBOARD", 5)
+
+        rospy.loginfo("run mission")
+
+        while True:
+            key = input("Set new command!\n")
+            ret = self.on_release(key)
+            if ret:
+                break
+
+
+if __name__ == '__main__':
+    import rostest
+    rospy.init_node('test_node', anonymous=True)
+
+    rostest.rosrun("navigation", 'mavros_offboard_posctl_test',
+                   MavrosOffboardPosctlTest)
