40399a759814a05a9a0ec06d21e2c13203ef24d4
PawelMiera <pawelmiera@gmail.com>
2022-06-21 22:10:38 +0200

    test

--- python/speed_test.py
+++ python/speed_test.py
@@ -2,7 +2,7 @@ import time
 
 import rospy
 from sensor_msgs.msg import LaserScan
-import cv2
+#import cv2
 from math import pi, sin, cos
 import numpy as np
 from fast_process import preprocess_fast
@@ -94,27 +94,7 @@ class Laser:
 
         self.laser_ranges = data
 
-    def render(self, mode='human'):
-        background = np.zeros((self.window_size[0], self.window_size[1], 3), np.uint8)
-        background[:] = (0, 255, 0)
 
-        for i in range(self.laser_resolution):
-            A = int(self.laser_ranges[i] * self.pixels_per_meter * sin(i * self.laser_angle_per_step)) + \
-                self.window_size_half[0]
-            B = -int(self.laser_ranges[i] * self.pixels_per_meter * cos(i * self.laser_angle_per_step)) + \
-                self.window_size_half[1]
-            cv2.line(background, self.window_size_half, (A, B), (0, 0, 255), 1)
-            cv2.circle(background, (A, B), 2, (0, 0, 255), -1)
-
-        drone_x_min = self.window_size_half[0] - 15
-        drone_y_min = self.window_size_half[1] - 10
-        background = cv2.rectangle(background, (drone_y_min, drone_x_min), (drone_y_min + 20, drone_x_min + 30),
-                                   (0, 0, 0), -1)
-        background = cv2.rectangle(background, (drone_y_min, drone_x_min), (drone_y_min + 20, drone_x_min + 5),
-                                   (255, 0, 0), -1)
-
-        cv2.imshow("game", background)
-        return cv2.waitKey(1)
 
 
 laser = Laser()
