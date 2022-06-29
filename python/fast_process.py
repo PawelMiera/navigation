5806e116a082974e6c98b000e53b1afb7c3c0bf0
PawelMiera <pawelmiera@gmail.com>
2022-06-29 14:05:18 +0200

    gps

--- python/fast_process.py
+++ python/fast_process.py
@@ -22,6 +22,8 @@ def preprocess_fast(laser_ranges, laser_resolution, laser_max_range, laser_min_r
             if curr_sum != 0:
                 data2[i] = curr_sum / count
 
+            data2[i] = data2[i] - 0.1
+
             if data2[i] > laser_max_range:
                 data2[i] = laser_max_range
             elif data2[i] < laser_min_range:
